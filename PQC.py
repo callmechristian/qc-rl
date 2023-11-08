c4e3a2cd08223e5958f1a2298694e420ea0fed82
callmechristian <siman.christian@gmail.com>
2023-11-07 19:39:51 -0500

    classification

---
+++ PQC.py
@@ -0,0 +1,165 @@
+import tensorflow as tf
+import cirq, sympy
+import tensorflow_quantum as tfq
+import numpy as np
+from functools import reduce
+
+## SET TF ERROR LEVEL ##
+tf.get_logger().setLevel('ERROR')
+## ================== ##
+
+## PARAMS ##
+n_qubits = 4 # Dimension of the state vectors in CartPole
+n_layers = 5 # Number of layers in the PQC
+n_actions = 2 # Number of actions in CartPole
+
+qubits = cirq.GridQubit.rect(1, n_qubits)
+
+ops = [cirq.Z(q) for q in qubits]
+observables = [reduce((lambda x, y: x * y), ops)] # Z_0*Z_1*Z_2*Z_3
+## PARAMS ##
+
+def one_qubit_rotation(qubit, symbols):
+    """
+    Returns Cirq gates that apply a rotation of the bloch sphere about the X,
+    Y and Z axis, specified by the values in `symbols`.
+    """
+    return [cirq.rx(symbols[0])(qubit),
+            cirq.ry(symbols[1])(qubit),
+            cirq.rz(symbols[2])(qubit)]
+
+def entangling_layer(qubits):
+    """
+    Returns a layer of CZ entangling gates on `qubits` (arranged in a circular topology).
+    """
+    cz_ops = [cirq.CZ(q0, q1) for q0, q1 in zip(qubits, qubits[1:])]
+    cz_ops += ([cirq.CZ(qubits[0], qubits[-1])] if len(qubits) != 2 else [])
+    return cz_ops
+
+def generate_circuit(qubits, n_layers):
+    """Prepares a data re-uploading circuit on `qubits` with `n_layers` layers."""
+    # Number of qubits
+    n_qubits = len(qubits)
+
+    # Sympy symbols for variational angles
+    params = sympy.symbols(f'theta(0:{3*(n_layers+1)*n_qubits})')
+    params = np.asarray(params).reshape((n_layers + 1, n_qubits, 3))
+
+    # Sympy symbols for encoding angles
+    inputs = sympy.symbols(f'x(0:{n_layers})'+f'_(0:{n_qubits})')
+    inputs = np.asarray(inputs).reshape((n_layers, n_qubits))
+
+    # Define circuit
+    circuit = cirq.Circuit()
+    for l in range(n_layers):
+        # Variational layer
+        circuit += cirq.Circuit(one_qubit_rotation(q, params[l, i]) for i, q in enumerate(qubits))
+        circuit += entangling_layer(qubits)
+        # Encoding layer
+        circuit += cirq.Circuit(cirq.rx(inputs[l, i])(q) for i, q in enumerate(qubits))
+
+    # Last varitional layer
+    circuit += cirq.Circuit(one_qubit_rotation(q, params[n_layers, i]) for i,q in enumerate(qubits))
+
+    return circuit, list(params.flat), list(inputs.flat)
+
+
+class ReUploadingPQC(tf.keras.layers.Layer):
+    """
+    Performs the transformation (s_1, ..., s_d) -> (theta_1, ..., theta_N, lmbd[1][1]s_1, ..., lmbd[1][M]s_1,
+        ......., lmbd[d][1]s_d, ..., lmbd[d][M]s_d) for d=input_dim, N=theta_dim and M=n_layers.
+    An activation function from tf.keras.activations, specified by `activation` ('linear' by default) is
+        then applied to all lmbd[i][j]s_i.
+    All angles are finally permuted to follow the alphabetical order of their symbol names, as processed
+        by the ControlledPQC.
+    """
+
+    def __init__(self, qubits, n_layers, observables, activation="linear", name="re-uploading_PQC"):
+        super(ReUploadingPQC, self).__init__(name=name)
+        self.n_layers = n_layers
+        self.n_qubits = len(qubits)
+
+        circuit, theta_symbols, input_symbols = generate_circuit(qubits, n_layers)
+
+        theta_init = tf.random_uniform_initializer(minval=0.0, maxval=np.pi)
+        self.theta = tf.Variable(
+            initial_value=theta_init(shape=(1, len(theta_symbols)), dtype="float32"),
+            trainable=True, name="thetas"
+        )
+
+        lmbd_init = tf.ones(shape=(self.n_qubits * self.n_layers,))
+        self.lmbd = tf.Variable(
+            initial_value=lmbd_init, dtype="float32", trainable=True, name="lambdas"
+        )
+
+        # Define explicit symbol order.
+        symbols = [str(symb) for symb in theta_symbols + input_symbols]
+        self.indices = tf.constant([symbols.index(a) for a in sorted(symbols)])
+
+        self.activation = activation
+        self.empty_circuit = tfq.convert_to_tensor([cirq.Circuit()])
+        self.computation_layer = tfq.layers.ControlledPQC(circuit, observables)        
+
+    def call(self, inputs):
+        # inputs[0] = encoding data for the state.
+        batch_dim = tf.gather(tf.shape(inputs[0]), 0)
+        tiled_up_circuits = tf.repeat(self.empty_circuit, repeats=batch_dim)
+        tiled_up_thetas = tf.tile(self.theta, multiples=[batch_dim, 1])
+        tiled_up_inputs = tf.tile(inputs[0], multiples=[1, self.n_layers])
+        scaled_inputs = tf.einsum("i,ji->ji", self.lmbd, tiled_up_inputs)
+        squashed_inputs = tf.keras.layers.Activation(self.activation)(scaled_inputs)
+
+        joined_vars = tf.concat([tiled_up_thetas, squashed_inputs], axis=1)
+        joined_vars = tf.gather(joined_vars, self.indices, axis=1)
+
+        return self.computation_layer([tiled_up_circuits, joined_vars])
+
+class ReUploadingPQC(tf.keras.layers.Layer):
+    """
+    Performs the transformation (s_1, ..., s_d) -> (theta_1, ..., theta_N, lmbd[1][1]s_1, ..., lmbd[1][M]s_1,
+        ......., lmbd[d][1]s_d, ..., lmbd[d][M]s_d) for d=input_dim, N=theta_dim and M=n_layers.
+    An activation function from tf.keras.activations, specified by `activation` ('linear' by default) is
+        then applied to all lmbd[i][j]s_i.
+    All angles are finally permuted to follow the alphabetical order of their symbol names, as processed
+        by the ControlledPQC.
+    """
+
+    def __init__(self, qubits, n_layers, observables, activation="linear", name="re-uploading_PQC"):
+        super(ReUploadingPQC, self).__init__(name=name)
+        self.n_layers = n_layers
+        self.n_qubits = len(qubits)
+
+        circuit, theta_symbols, input_symbols = generate_circuit(qubits, n_layers)
+
+        theta_init = tf.random_uniform_initializer(minval=0.0, maxval=np.pi)
+        self.theta = tf.Variable(
+            initial_value=theta_init(shape=(1, len(theta_symbols)), dtype="float32"),
+            trainable=True, name="thetas"
+        )
+
+        lmbd_init = tf.ones(shape=(self.n_qubits * self.n_layers,))
+        self.lmbd = tf.Variable(
+            initial_value=lmbd_init, dtype="float32", trainable=True, name="lambdas"
+        )
+
+        # Define explicit symbol order.
+        symbols = [str(symb) for symb in theta_symbols + input_symbols]
+        self.indices = tf.constant([symbols.index(a) for a in sorted(symbols)])
+
+        self.activation = activation
+        self.empty_circuit = tfq.convert_to_tensor([cirq.Circuit()])
+        self.computation_layer = tfq.layers.ControlledPQC(circuit, observables)        
+
+    def call(self, inputs):
+        # inputs[0] = encoding data for the state.
+        batch_dim = tf.gather(tf.shape(inputs[0]), 0)
+        tiled_up_circuits = tf.repeat(self.empty_circuit, repeats=batch_dim)
+        tiled_up_thetas = tf.tile(self.theta, multiples=[batch_dim, 1])
+        tiled_up_inputs = tf.tile(inputs[0], multiples=[1, self.n_layers])
+        scaled_inputs = tf.einsum("i,ji->ji", self.lmbd, tiled_up_inputs)
+        squashed_inputs = tf.keras.layers.Activation(self.activation)(scaled_inputs)
+
+        joined_vars = tf.concat([tiled_up_thetas, squashed_inputs], axis=1)
+        joined_vars = tf.gather(joined_vars, self.indices, axis=1)
+
+        return self.computation_layer([tiled_up_circuits, joined_vars])
---
+++ train.py
@@ -0,0 +1,74 @@
+from PolicyGradientRL import *
+from PIL import Image
+import os
+
+env_name = "CartPole-v1"
+
+model = generate_model_policy(qubits, n_layers, n_actions, 1.0, observables)
+episode_reward_history = []
+
+def train(reward_target=500.0, realtime_render=False):
+    episode_reward_history = []
+    # Start training the agent
+    for batch in range(n_episodes // batch_size):
+        # Gather episodes
+        episodes = gather_episodes(state_bounds, n_actions, model, batch_size, env_name)
+
+        # Group states, actions and returns in numpy arrays
+        states = np.concatenate([ep['states'] for ep in episodes])
+        actions = np.concatenate([ep['actions'] for ep in episodes])
+        rewards = [ep['rewards'] for ep in episodes]
+        returns = np.concatenate([compute_returns(ep_rwds, gamma) for ep_rwds in rewards])
+        returns = np.array(returns, dtype=np.float32)
+
+        id_action_pairs = np.array([[i, a] for i, a in enumerate(actions)])
+
+        # Update model parameters.
+        reinforce_update(states, id_action_pairs, returns, model)
+
+        # Store collected rewards
+        for ep_rwds in rewards:
+            episode_reward_history.append(np.sum(ep_rwds))
+
+        avg_rewards = np.mean(episode_reward_history[-10:])
+
+        print('Finished episode', (batch + 1) * batch_size,
+            'Average rewards: ', avg_rewards)
+
+        if avg_rewards >= reward_target:
+            break
+
+        if realtime_render:
+            env = gym.make(env_name)
+            state = env.reset()
+            for t in range(500):
+                env.render()
+                policy = model([tf.convert_to_tensor([state/state_bounds])])
+                action = np.random.choice(n_actions, p=policy.numpy()[0])
+                state, _, done, _ = env.step(action)
+                if done:
+                    break
+            env.close()
+
+    return episode_reward_history
+
+def export(history: list, dir="./images", note=""):
+    if len(history) == 0:
+        raise IndexError("Train a model first!")
+
+    nr = len([f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))])
+
+    env = gym.make(env_name)
+    state = env.reset()
+    frames = []
+    for t in range(500):
+        im = Image.fromarray(env.render(mode='rgb_array'))
+        frames.append(im)
+        policy = model([tf.convert_to_tensor([state/state_bounds])])
+        action = np.random.choice(n_actions, p=policy.numpy()[0])
+        state, _, done, _ = env.step(action)
+        if done:
+            break
+    env.close()
+    frames[1].save(dir + '/' + str(nr) + '_gym_' + str(env_name) + str(note) + '.gif',
+                save_all=True, append_images=frames[2:], optimize=False, duration=40, loop=0)
