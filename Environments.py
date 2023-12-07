60f364c8d631ea066eb1836d1c9ccfdd2a73e44f
callmechristian <siman.christian@gmail.com>
2023-12-07 14:54:43 -0500

    report img and comment consistency

--- Environments.py
+++ Environments.py
@@ -8,13 +8,12 @@ class Environment(ABC):
     n_layers = None
     n_actions = None
     state_bounds = None
-    # https://www.gymlibrary.dev/environments/classic_control/acrobot/
 
 class AcroBot(Environment):
     env_name="Acrobot-v1"
     max_steps=500
     n_qubits = 6
-    n_layers = 15 # Number of layers in the PQC
+    n_layers = 8 # Number of layers in the PQC
     n_actions = 3
     state_bounds = np.array([0.3,0.7,0.3,0.7,12,28])
     # https://www.gymlibrary.dev/environments/classic_control/acrobot/
@@ -35,3 +34,4 @@ class AtariBreakout(Environment):
     n_layers = 5 # Number of layers in the PQC
     n_actions = 4 # restricted from 16
     state_bounds = 255
+    # https://www.gymlibrary.dev/environments/atari/breakout/
